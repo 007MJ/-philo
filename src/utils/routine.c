e6af20897b801bddbbce358dc6102b8c84545898
007MJ <k.mijiza@gmail.com>
2023-12-08 20:11:00 -0500

    entre de fix

--- src/utils/routine.c
+++ src/utils/routine.c
@@ -10,7 +10,7 @@ void	*routine(void *tab_rond)
 
 	same_tab = (t_circle *)tab_rond;
 	pthread_mutex_lock(&same_tab->tab_socrates->mutex);
-		printf("--> %p\n", &same_tab->tab_socrates->id);
+		printf("--> %d\n", same_tab->tab_socrates->id);
 	pthread_mutex_unlock(&same_tab->tab_socrates->mutex);
 	printf("Test from tread\n");
 	return (NULL);
