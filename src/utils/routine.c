47c40e28905f9b6eeb2888939070695a683df007
007MJ <k.mijiza@gmail.com>
2023-12-13 12:21:01 -0500

    next

--- src/utils/routine.c
+++ src/utils/routine.c
@@ -9,12 +9,5 @@ void	*routine(void *real_philo)
 	t_philo	*fake_philo;
 
 	fake_philo = (t_philo *)real_philo;
-	if (fake_philo->id >= 0)
-	{
-		// printf("id %d next fork %p\n", fake_philo->id, fake_philo->next_mutex);
-		// printf("-------\n");
-		// printf("id %d next fork %p\n", fake_philo->id, &fake_philo->mutex);
-		printf("id %d time to die %d\n", fake_philo->id, fake_philo->tab_to_eat->time_to_die);
-	}
 	return (NULL);
 }
